cddca604fe280c38c5769011d97f7c3da02e78fe
Abigail <goldrnsun@gmail.com>
2026-05-27 19:06:09 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "NixOS configuration with Noctalia";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, ... }: {
+    nixosConfigurations.tgirl-nix-swag = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+	./configuration.nix
+	./noctilia.nix
+      ];
+    };
+  };
+}
