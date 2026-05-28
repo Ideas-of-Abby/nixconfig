ce67f23620c0194928de08d3a1916aa9a2fa3c05
Abigail <goldrnsun@gmail.com>
2026-05-27 21:56:02 -0400

    now please?

--- nixupdate.nix
+++ nixupdate.nix
@@ -3,7 +3,7 @@ with import <nixpkgs> {};
 writeShellScriptBin "nixupdate" ''
 	git add .
 	IFS="\n" read -p "enter commit message : " commit_message
-	git commit -m ${#commit_message}
+	git commit -m "$commit_message"
 	git push -u origin main
 	sudo nix flake update; sudo nixos-rebuild switch --flake --impure
 	''
