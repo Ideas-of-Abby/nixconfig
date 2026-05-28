e52fbb75603104cc3fb2efd07d20ca37752f5071
Abigail <goldrnsun@gmail.com>
2026-05-27 21:58:52 -0400

    adding contition for commiting

--- nixupdate.nix
+++ nixupdate.nix
@@ -1,9 +1,11 @@
 with import <nixpkgs> {};
     
 writeShellScriptBin "nixupdate" ''
+	read -p "do you want to commit? [Y/N]" committing
+	if $committing = "Y" [
 	git add .
 	IFS="\n" read -p "enter commit message : " commit_message
 	git commit -m "$commit_message"
-	git push -u origin main
+	git push -u origin main ]
 	sudo nix flake update; sudo nixos-rebuild switch --flake --impure
 	''
