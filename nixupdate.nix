cb83258fa5bf58a9d581978113d3b127747e6c25
Abigail <goldrnsun@gmail.com>
2026-05-27 22:01:56 -0400

    how bout now?

--- nixupdate.nix
+++ nixupdate.nix
@@ -2,10 +2,13 @@ with import <nixpkgs> {};
     
 writeShellScriptBin "nixupdate" ''
 	read -p "do you want to commit? [Y/N]" committing
-	if $committing = "Y" [
+	if [$committing == "Y" ]; then
 	git add .
 	IFS="\n" read -p "enter commit message : " commit_message
 	git commit -m "$commit_message"
 	git push -u origin main ]
+	else
+	echo "not committing changes"
+	fi
 	sudo nix flake update; sudo nixos-rebuild switch --flake --impure
 	''
