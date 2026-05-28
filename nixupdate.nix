4002112984838f49e8b2d07da6fc2a0cb1ecd1ad
Abigail <goldrnsun@gmail.com>
2026-05-27 20:56:18 -0400

    still trying for the updater script 4

--- nixupdate.nix
+++ nixupdate.nix
@@ -1,12 +1,12 @@
 {pkgs, inputs, ...}: 
 {
 	environment.systemPackages = with pkgs; [
-		writeShellScriptBin "nixupdate" ''
+		import (writeShellScriptBin "nixupdate" ''
 		git add .
 		read -sp "enter commit message : " commit_message
 		git commit -m $commit_message
 		git push -u origin main
 		sudo nix flake update; sudo nixos-rebuild switch --flake
-		''
+		'')
 	];
 }
